34e50c5547bda47822b951bcecded516751a609c
Kyle Chin <144987241+kjc33@users.noreply.github.com>
2023-11-06 18:56:14 -0800

    Updated Mobile Menu with Close Button

---
+++ script.js
@@ -0,0 +1,25 @@
+const mobileMenuBurger = document.getElementById("mobileMenuBurger");
+const mobileNavItems = document.getElementById("mobileMenuNavItems");
+const mobileSiteNav = document.getElementById("siteNav");
+const mobileMenuCloseBtn = document.getElementById("closeBtn");
+
+// Mobile Menu Nav Items
+
+mobileMenuBurger.addEventListener("click", displayMobileMenu);
+
+function displayMobileMenu() {
+    // Shows Full Screen Mobile Menu
+    mobileNavItems.style.display = "block";
+
+    // Adds and Displays Navigation
+    mobileNavItems.appendChild(mobileSiteNav);
+    mobileSiteNav.style.display = "flex";
+}
+
+// Mobile Menu Close Button
+
+mobileMenuCloseBtn.addEventListener("click", closeMobileMenu);
+
+function closeMobileMenu() {
+    mobileNavItems.style.display = "none";
+}
